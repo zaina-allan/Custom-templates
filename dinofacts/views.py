1f0d90b0d8007321f21c0b7661a3895299003c21
zaina-allan <75915075+zaina-allan@users.noreply.github.com>
2022-12-20 18:23:10 +0200

    Add files via upload

---
+++ dinofacts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from datetime import datetime
+from django.shortcuts import render
+def show_dino(request, name):
+    data = {
+    "dinosaurs": [
+    "Tyrannosaurus",
+    "Stegosaurus",
+    "Raptor",
+    "Triceratops",
+    ],
+    "now": datetime.now(),
+      }
+    return render(request, name + ".html", data)
